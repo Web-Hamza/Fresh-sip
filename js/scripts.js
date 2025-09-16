1eedf24f225fcdeec8f53938507b1b524a635925
Muhammad Hamza <m.hamza19536@gmail.com>
2025-09-16 11:30:02 +0500

    I think websitse is complete

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+
+// // Lightbox functionality for gallery images
+
+//   $(function(){
+//     $('#galleryGrid img').on('click', function(){
+//       const src = $(this).attr('src');
+//       $('#lightboxImg').attr('src', src);
+//       const modal = new bootstrap.Modal(document.getElementById('lightboxModal'));
+//       modal.show();
+//     });
+//   });
